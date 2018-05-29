82e1a6b6935f95f01cc7d0db12ab933003c11206
Simon Helge <32535146+simonhelge@users.noreply.github.com>
2018-05-29 14:02:20 +0200

    Add files via upload

--- earlybird.js
+++ earlybird.js
@@ -1,12 +1,15 @@
+/* Nedenstående JS definerer de to tællere, der tæller antal personer og antal menuer */
 var count = 1;
 var countEl = document.getElementById("count");
 
 function plus() {
+    console.log('der klikkes paa plus, værdi af count = ' + count)
     count++;
     countEl.value = count;
 }
 
 function minus() {
+    console.log('der klikkes paa minus, værdi af count = ' + count)
     if (count > 1) {
         count--;
         countEl.value = count;
@@ -14,6 +17,26 @@ function minus() {
 }
 
 
+/* Nedenstående JS definerer de to tællere, der tæller antal personer og antal menuer */
+var count1 = 1;
+var countEl1 = document.getElementById("count1");
+
+function plus1() {
+    count1++; // count1 = count1 + 1
+    console.log('der klikkes paa plus1: ' + count1)
+    countEl1.value = count1;
+}
+
+function minus1() {
+    console.log('der klikkes paa minus1: ' + count1)
+    if (count1 > 1) {
+        count1--; // count1 = count1 - 1
+        countEl1.value = count1;
+    }
+}
+
+
+/* Nedenstående JS definerer det interaktive map */
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2ltb25oZWxnZSIsImEiOiJjamZqY3h0ZjE1OXpiMzJwbmcxcnB0a2tlIn0.amIGmp8oupjXrtgL4WaYjw';
 
 var map = new mapboxgl.Map({
@@ -24,5 +47,6 @@ var map = new mapboxgl.Map({
     bearing: 27,
     pitch: 20
 });
+
 
 
